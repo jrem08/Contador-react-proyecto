dfc9a4f1ecb7353624148205866498dff28c0a41
Jrem08 <medrandajuan843@gmail.com>
2025-08-19 21:01:31 -0500

    Proyecto de React contador con Vite y Tailwind, primer proyecto con el profe Nicolas

---
+++ Proyecto-de-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './Counter'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Mi primer proyecto React</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
